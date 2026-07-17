e86195892539afdeeb5dfc79d87ad58c963cb6fb
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-17 22:37:03 +0900

    refactor: API 서블릿 접두사 상수 분리
    --- 본문 ---
    - `WebMvcConfig` 내부에 정의되어 있던 `/api` 서블릿 접두사를 `WebConstants` 클래스로 분리했습니다.
    - 이를 통해 API 접두사에 대한 단일 진실 공급원을 확보하고, 향후 변경 및 관리를 용이하게 합니다.

--- src/main/java/io/github/metdaisy/amaazon/global/web/config/WebMvcConfig.java
+++ src/main/java/io/github/metdaisy/amaazon/global/web/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package io.github.metdaisy.amaazon.global.web.config;
 
+import io.github.metdaisy.amaazon.global.web.constant.WebConstants;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
@@ -9,11 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-  private final String servletPrefix = "/api";
-
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer) {
-    configurer.addPathPrefix(servletPrefix,
+    configurer.addPathPrefix(WebConstants.SERVLET_PREFIX,
             HandlerTypePredicate.forAnnotation(RestController.class));
   }
 }
---
+++ src/main/java/io/github/metdaisy/amaazon/global/web/constant/WebConstants.java
@@ -0,0 +1,10 @@
+package io.github.metdaisy.amaazon.global.web.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class WebConstants {
+
+  public static String SERVLET_PREFIX = "/api";
+}
