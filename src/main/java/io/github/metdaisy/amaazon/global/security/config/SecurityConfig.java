4ad7aa8bdb846d33bb26b2f5d1245754091f30ef
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-17 22:37:18 +0900

    feat: 초기 Spring Security 설정 및 비밀번호 인코더 추가
    --- 본문 ---
    - Spring Security의 핵심 설정인 SecurityFilterChain을 구성하는 SecurityConfig를 추가했습니다.
    - REST API 기반 애플리케이션에 적합하도록 CSRF를 비활성화하고 세션 관리 정책을 STATELESS로 설정했습니다.
    - 폼 로그인 및 로그아웃 엔드포인트를 정의하고, 사용자 이름 및 비밀번호 파라미터를 설정했습니다.
    - Swagger UI, Actuator, 로그인, 회원가입 등 공개적으로 접근 가능한 GET 및 POST 경로를 설정하여 인증 없이 접근을 허용했습니다.
    - 비밀번호 인코딩을 위한 PasswordEncoderConfig를 추가하고, PasswordEncoderFactories.createDelegatingPasswordEncoder()를 사용하여 유연한 비밀번호 인코더 빈을 제공합니다.
    - 보안 관련 상수들을 SecurityConstants에 모아 관리하여 코드의 가독성과 유지보수성을 높였습니다.

---
+++ src/main/java/io/github/metdaisy/amaazon/global/security/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package io.github.metdaisy.amaazon.global.security.config;
+
+import io.github.metdaisy.amaazon.global.security.constant.SecurityConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(form ->
+                    form.loginProcessingUrl(SecurityConstants.LOGIN_URL)
+                            .usernameParameter(SecurityConstants.USERNAME_PARAMETER)
+                            .passwordParameter(SecurityConstants.PASSWORD_PARAMETER))
+            .logout(logout ->
+                    logout.logoutUrl(SecurityConstants.LOGOUT_URL)
+                            .permitAll())
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                    auth.requestMatchers(HttpMethod.GET, SecurityConstants.PUBLIC_GET_PATHS).permitAll()
+                            .requestMatchers(HttpMethod.POST, SecurityConstants.PUBLIC_POST_PATHS).permitAll()
+                            .anyRequest().authenticated())
+            .build();
+  }
+}
