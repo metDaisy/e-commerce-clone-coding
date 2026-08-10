718a15ea7ac93ac8c267237429de9ce77d7e091e
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-08-11 00:16:04 +0900

    refactor: 상품 관련 DTO, 엔티티, 서비스, 레포지토리 삭제
    --- 본문 ---
    - 상품 생성 요청 DTO (ProductCreateRequest) 삭제
    - 상품 응답 DTO (ProductResponse) 삭제
    - 상품 매퍼 (ProductMapper) 삭제
    - 상품 서비스 (ProductService) 삭제
    - 상품 엔티티 (Product) 삭제
    - 상품 상태 Enum (ProductStatus) 삭제
    - 상품 태그 엔티티 (ProductTag) 삭제
    - 상품 관련 에러 코드 (ProductErrorCode) 삭제
    - 상품 관련 예외 클래스 (ProductException) 삭제
    - 상품 Repository 인터페이스 (ProductRepository) 삭제
    - 상품 Repository 구현체 (ProductJpaRepository) 삭제
    - 상품 컨트롤러 (ProductController) 삭제
    
    이 파일들은 현재 프로젝트 구조에서 사용되지 않거나, 향후 다른 방식으로 구현될 예정이므로 삭제합니다.

--- src/main/java/io/github/metdaisy/amaazon/product/domain/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.metdaisy.amaazon.product.domain.repository;
-
-import io.github.metdaisy.amaazon.common.jpa.repository.DomainRepository;
-import io.github.metdaisy.amaazon.product.domain.entity.Product;
-
-public interface ProductRepository extends DomainRepository<Product> {
-
-}
