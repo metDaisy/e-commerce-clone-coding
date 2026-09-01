5c03258e95771d447fa14e34e81121c5d35106a4
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-09-01 23:06:51 +0900

    refactor: 커서 관련 불필요한 코드 삭제
    - CursorCodec 인터페이스, 관련 예외 클래스, DTO, HMAC 구현체 및 설정이 삭제되었습니다.
    - 해당 기능이 현재 프로젝트에서 사용되지 않아 불필요한 코드로 판단되어 제거되었습니다.

--- src/main/java/io/github/metdaisy/amaazon/common/cursor/CursorCodec.java
+++
@@ -1,8 +0,0 @@
-package io.github.metdaisy.amaazon.common.cursor;
-
-public interface CursorCodec {
-
-  String encode(Object payload);
-
-  <T> T decode(String cursor, Class<T> payloadType);
-}
--- src/main/java/io/github/metdaisy/amaazon/common/cursor/package-info.java
+++
@@ -1,4 +0,0 @@
-@NamedInterface("cursor")
-package io.github.metdaisy.amaazon.common.cursor;
-
-import org.springframework.modulith.NamedInterface;
